534307d3808b45875a1643f13bbf080b2076a8f5
Amin Mustafa Boankod <amin.m.boankod@gmail.com>
2023-02-15 18:35:55 +0100

    finish Exam

--- src/main/java/edu/craptocraft/enzinium/App.java
+++ src/main/java/edu/craptocraft/enzinium/App.java
@@ -327,19 +327,19 @@ public class App {
     //     // verifica que Morty no tiene dinero para comprar una entrada
     //     morty.send(ricknillos, 3d);
 
-    //     System.out.println("Morty no paga suficientes EZI por una entrada" +
-    //                         "\n => sigue teniendo 5 entradas: " 
-    //                             + ricknillos.balanceOf(morty.getPK())
-    //                             + " "
-    //                             + ricknillos.symbol());
+        System.out.println("Morty no paga suficientes EZI por una entrada" +
+                            "\n => sigue teniendo 5 entradas: " 
+                                + ricknillos.balanceOf(morty.getPK())
+                                + " "
+                                + ricknillos.symbol());
 
-    //     System.out.println("\n" + "y Rick no recibe EZI => sigue con 10: " 
-    //                                     + rick.getBalance()
-    //                                     + "\n");
+        System.out.println("\n" + "y Rick no recibe EZI => sigue con 10: " 
+                                        + rick.getBalance()
+                                        + "\n");
 
-    //     System.out.println("y Rick sigue con sus 94 entradas: " 
-    //                                         + ricknillos.balanceOf(rick.getPK())
-    //                                         + " "
-    //                                         + ricknillos.symbol());
+        System.out.println("y Rick sigue con sus 94 entradas: " 
+                                            + ricknillos.balanceOf(rick.getPK())
+                                            + " "
+                                            + ricknillos.symbol());
     }
 }
